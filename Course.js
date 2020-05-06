596ab9cf44e07b9c2dd2ad2b82f8f528f77527e7
DavittBarry <63282613+DavittBarry@users.noreply.github.com>
2020-05-06 14:34:06 +0300

    Delete Course.js

--- Course.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Course = ({course}) =>  {
-    const partname = course.map(part => <li key={part.id}>{part.name} {part.exercises}</li>) 
-    console.log(partname)
-   return (<div>{partname}</div>)
-  }
-
-  export default Course
